efcf8a950e385a397d1d34f16c16d45865357ea8
Askill <ofjokg@gmail.com>
2022-01-01 12:52:36 +0100

    updated style

--- x.py
+++ x.py
@@ -1,6 +1,6 @@
 import Star
 
 crawler = Star.Crawler()
-crawler.run("https://www.patricematz.de/", 5000)
+crawler.run("https://www.google.de/", 5000)
 print(crawler.getNodesEdges())
 crawler.draw()
